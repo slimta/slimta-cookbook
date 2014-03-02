8ceb3bc6905c0f634738a7e7f9da00fe6c521be7
Ian Good <ian.good@rackspace.com>
2014-03-01 20:56:16 -0500

    implemented the slimta_app LWRP

---
+++ providers/app.rb
@@ -0,0 +1,143 @@
+#
+# Cookbook Name:: slimta
+# Provider:: slimta_app
+#
+# Copyright (c) 2014 Ian C. Good
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+def whyrun_supported?
+  true
+end
+
+use_inline_resources
+
+action :create do
+  updated = false
+  app_name = new_resource.app_name
+  executable = new_resource.executable || \
+    ::File.join(node['slimta']['virtualenv'], 'bin/slimta')
+  etc_dir = new_resource.etc_dir || \
+    ::File.join(node['slimta']['virtualenv'], 'etc')
+
+  app_cfg_file = new_resource.conf_files.fetch('app', "#{app_name}.conf")
+  log_cfg_file = new_resource.conf_files.fetch('logging', 'logging.conf')
+  rules_cfg_file = new_resource.conf_files.fetch('rules', 'rules.conf')
+
+  process_cfg = new_resource.process || node['slimta']['defaults']['process']
+  logging_cfg = new_resource.logging || node['slimta']['defaults']['logging']
+  edge_cfg = new_resource.edge || node['slimta']['defaults']['edge']
+  rules_cfg = new_resource.rules || node['slimta']['defaults']['rules']
+  queue_cfg = new_resource.queue || node['slimta']['defaults']['queue']
+  relay_cfg = new_resource.relay || node['slimta']['defaults']['relay']
+
+  cfg_dir = directory etc_dir do
+    mode 0755
+    recursive true
+  end
+  updated ||= cfg_dir.updated_by_last_action?
+
+  log_dir = directory logging_cfg['directory'] do
+    mode 0755
+    recursive true
+  end
+  updated ||= log_dir.updated_by_last_action?
+
+  group = group process_cfg['group'] do
+    action :create
+  end
+  updated ||= group.updated_by_last_action?
+
+  user = user process_cfg['user'] do
+    comment "#{ app_name } user"
+    gid process_cfg['group']
+    shell '/bin/false'
+    action :create
+  end
+  updated ||= user.updated_by_last_action?
+
+  init = template ::File.join('/etc/init.d', app_name) do
+    cookbook new_resource.cookbook
+    source 'slimta.init.erb'
+    mode 0755
+    variables({
+      :app_name => app_name,
+      :app_cfg_file => ::File.join(etc_dir, app_cfg_file),
+      :executable => executable
+    })
+    action :create
+  end
+  updated ||= init.updated_by_last_action?
+
+  service = service app_name do
+    supports :status => true, :start => true, :stop => true,
+      :restart => true, :reload => true
+    action :nothing
+  end
+  updated ||= service.updated_by_last_action?
+
+  app_cfg = template ::File.join(etc_dir, app_cfg_file) do
+    cookbook new_resource.cookbook
+    source 'app.conf.erb'
+    mode 0644
+    variables({
+      :app_name => app_name,
+      :log_cfg_file => log_cfg_file,
+      :rules_cfg_file => rules_cfg_file,
+      :process_cfg => process_cfg,
+      :logging_cfg => logging_cfg,
+      :edge_cfg => edge_cfg,
+      :rules_cfg => rules_cfg,
+      :queue_cfg => queue_cfg,
+      :relay_cfg => relay_cfg,
+    })
+    notifies :restart, "service[#{ app_name }]"
+    action :create
+  end
+  updated ||= app_cfg.updated_by_last_action?
+
+  log_cfg = template ::File.join(etc_dir, log_cfg_file) do
+    cookbook new_resource.cookbook
+    source 'logging.conf.erb'
+    mode 0644
+    variables({
+      :app_name => app_name,
+      :logging_cfg => logging_cfg,
+    })
+    action :create
+  end
+  updated ||= log_cfg.updated_by_last_action?
+
+  rules_cfg = template ::File.join(etc_dir, rules_cfg_file) do
+    cookbook new_resource.cookbook
+    source 'rules.conf.erb'
+    mode 0644
+    variables({
+      :app_name => app_name,
+      :rules_cfg => rules_cfg,
+    })
+    action :create
+  end
+  updated ||= rules_cfg.updated_by_last_action?
+
+  new_resource.updated_by_last_action(updated)
+end
+
+# vim:sw=2:ts=2:sts=2:et:ai:ft=ruby:
--- resources/app.rb
+++ resources/app.rb
@@ -1,11 +1,11 @@
 #
 # Cookbook Name:: slimta
-# Recipe:: default
+# Resource:: slimta_app
 #
 # Copyright (c) 2014 Ian C. Good
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the 'Software'), to deal
+# of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
@@ -14,7 +14,7 @@
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -23,33 +23,20 @@
 # THE SOFTWARE.
 #
 
-include_recipe 'python'
+actions :create
+default_action :create
 
-venv = node['slimta']['virtualenv']
-
-python_virtualenv venv do
-  action :create
-end
-
-packages = Set.new [
-  'python-slimta',
-  'python-slimta-spf',
-  'python-slimta-piperelay',
-  'python-slimta-diskstorage',
-  'python-slimta-redisstorage',
-  'python-slimta-celeryqueue',
-  'python-slimta-cloudstorage',
-  'slimta',
-] - node['slimta']['exclude']
-
-versions = node['slimta']['version_lock']
-
-packages.each do |pkg|
-  pkg_version = versions.fetch(pkg, nil)
-  python_pip pkg do
-    virtualenv venv
-    version pkg_version
-  end
-end
+attribute :app_name, :name_attribute => true, :kind_of => String,
+  :required => true
+attribute :executable, :kind_of => String
+attribute :etc_dir, :kind_of => String
+attribute :conf_files, :kind_of => Hash, :default => {}
+attribute :process, :kind_of => [Hash, NilClass], :default => nil
+attribute :logging, :kind_of => [Hash, NilClass], :default => nil
+attribute :edge, :kind_of => [Hash, NilClass], :default => nil
+attribute :rules, :kind_of => [Hash, NilClass], :default => nil
+attribute :queue, :kind_of => [Hash, NilClass], :default => nil
+attribute :relay, :kind_of => [Hash, NilClass], :default => nil
+attribute :cookbook, :kind_of => String
 
 # vim:sw=2:ts=2:sts=2:et:ai:ft=ruby:
